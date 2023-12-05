33eb79935ffcf0809f6f3dfdd58da9dfee29ec6b
Bhanu <145064788+bkanumur@users.noreply.github.com>
2023-12-04 23:50:39 -0500

    Add files via upload

---
+++ Project Weekly Progress 5/script.js
@@ -0,0 +1,22 @@
+<script>
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50, // Adjust the offset as needed
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+    
+document.getElementById("dark-mode-toggle").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+});
+
+</script>
